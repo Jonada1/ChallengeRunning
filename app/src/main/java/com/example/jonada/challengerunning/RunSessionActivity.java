203d3b9a2c19d0d19786a5ba25ec2b2e3fc5983a
jonada1 <jonada.ferracaku@gmail.com>
2019-04-29 21:07:00 +0300

    Add RunningSession Dynamic

--- app/src/main/java/com/example/jonada/challengerunning/RunSessionActivity.java
+++ app/src/main/java/com/example/jonada/challengerunning/RunSessionActivity.java
@@ -30,6 +30,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -42,22 +44,61 @@ public class RunSessionActivity extends FragmentActivity implements SensorEventL
     LocationListener locationListener;
     private boolean isInitiator;
 
+    private double kmH;
+    private double total_distance;
+
     private int timePassed;
     private boolean timerPaused;
     private int challengeId;
     final Timer timer = new Timer();
     int totalDuration;
+
     Button bt_pause;
     Button bt_play;
     Button bt_finish;
     Button bt_stop;
-
-
+    TextView speed;
     TextView steps;
+    TextView distance;
+
     SensorManager sensorManager;
     Sensor mSensor;
     private boolean isSensorPresent = false;
     boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        kmH = 0;
+        total_distance = 0;
+        timePassed = 55;
+        timerPaused = false;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_session);
+
+        Bundle extras = getIntent().getExtras();
+
+        challengeId = (int)extras.get("challengeId");
+        totalDuration = (int) extras.get("time");
+        isInitiator = (boolean)extras.get("isInitiator");
+
+        assignViews();
+        startMapAndSensor();
+        createTimer();
+    }
+    private void setKmh(double kmh) {
+        this.kmH = kmh;
+        speed.setText(String.format("%.2f", kmh));
+    }
+
+    private void setTotalDistance(double distance) {
+        this.total_distance = distance;
+        double totalSteps = (distance * 1250);
+
+        this.distance.setText(String.format( "%.2f", distance));
+        this.steps.setText(String.format("%d", (long) totalSteps));
+
+    }
 
     public void centreMapOnLocation(Location location, String title) {
 
@@ -80,55 +121,23 @@ public class RunSessionActivity extends FragmentActivity implements SensorEventL
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        timePassed = 55;
-        timerPaused = false;
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run_session);
-        Bundle extras = getIntent().getExtras();
-
-        challengeId = (int)extras.get("challengeId");
-        totalDuration = (int) extras.get("time");
-        isInitiator = (boolean)extras.get("isInitiator");
-
-        bt_pause = (Button) findViewById(R.id.bt_pause);
-        bt_play = (Button) findViewById(R.id.bt_play);
-        bt_finish = (Button) findViewById(R.id.btn_finish);
-        bt_stop = (Button) findViewById(R.id.btn_stop);
-        bt_pause.setVisibility(View.VISIBLE);
-        bt_play.setVisibility(View.GONE);
-        bt_finish.setVisibility(View.GONE);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_running);
-        mapFragment.getMapAsync(this);
-
-        steps = (TextView) findViewById(R.id.steps);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-                != null)
-        {
-            mSensor =
-                    sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            isSensorPresent = true;
-        }
-        else
-        {
-            isSensorPresent = false;
-        }
-
-
+    private void createTimer() {
         final TextView duration = (TextView) findViewById(R.id.textView_timer);
         final Handler handler = new Handler();
+        final Challenger challenger = getCurrentChallenger();
         timer.scheduleAtFixedRate(new TimerTask(){
             @Override
             public void run(){
                 if(!timerPaused) {
                     timePassed ++;
+
+                    Random random = new Random();
+                    final double kmH = (7.5 + random.nextDouble() * 2);
+                    setKmh(kmH);
+                    setTotalDistance(total_distance + kmH/3600);
+                    challenger.setDistance(total_distance + kmH/3600);
                     duration.setText(convertToMinuteSeconds(totalDuration - timePassed));
+
                 }
                 if((timePassed  >= totalDuration)) {
                     handler.post(new Runnable() {
@@ -144,9 +153,50 @@ public class RunSessionActivity extends FragmentActivity implements SensorEventL
                 }
             }
         },0,1000);
-
-
     }
+
+    private Challenger getCurrentChallenger() {
+        Challenger challenger = new Challenger(1, "Some default");
+        for(ChallengeData challenge : StaticMemoryDatabase.Challenges) {
+            if(challenge.getId() == challengeId) {
+                challenger = isInitiator ? challenge.getInitiator() : challenge.getReceiver();
+            }
+        }
+        return challenger;
+    }
+
+    private void startMapAndSensor() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_running);
+        mapFragment.getMapAsync(this);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+                != null)
+        {
+            mSensor =
+                    sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorPresent = true;
+        }
+        else
+        {
+            isSensorPresent = false;
+        }
+    }
+
+    private void assignViews() {
+        steps = (TextView) findViewById(R.id.steps);
+        speed = (TextView) findViewById(R.id.speed);
+        distance = (TextView) findViewById(R.id.distance);
+        bt_pause = (Button) findViewById(R.id.bt_pause);
+        bt_play = (Button) findViewById(R.id.bt_play);
+        bt_finish = (Button) findViewById(R.id.btn_finish);
+        bt_stop = (Button) findViewById(R.id.btn_stop);
+        bt_pause.setVisibility(View.VISIBLE);
+        bt_play.setVisibility(View.GONE);
+        bt_finish.setVisibility(View.GONE);
+    }
+
     public void onToggle(View view) {
         timerPaused = !timerPaused;
         bt_pause.setVisibility(bt_pause.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
@@ -171,6 +221,7 @@ public class RunSessionActivity extends FragmentActivity implements SensorEventL
         final TextView duration = (TextView) findViewById(R.id.textView_timer);
         duration.setText(convertToMinuteSeconds(totalDuration - timePassed));
     }
+
     private String convertToMinuteSeconds(int seconds) {
         int secondsRemainder = seconds % 60;
         int hours = seconds / 3600;
